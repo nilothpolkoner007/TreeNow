ead06c7e224ee94eab2aa372154784838e189710
nilothpolkoner007 <163440656+nilothpolkoner007@users.noreply.github.com>
2025-08-01 23:16:33 +0530

    Update Server.js

--- Treenow-main/Treenow-main/Backend/Server.js
+++ Treenow-main/Treenow-main/Backend/Server.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import connectDB from '../Config/db.js'; 
-import bonsaiRoutes from '../routes/bonsaiRoutes.js'; 
-import treeRoutes from '../routes/treeRoutes.js';
-import productRoutes from '../routes/productRoutes.js';
-import errorHandler from '../Midileware/errorMiddleware.js';
-import cartRoutes from '../routes/cartRoutes.js';
-import userRoutes from '../routes/userRoutes.js';
-import adminRoutes from '../routes/adminRouts.js';
-import diseaseRoutes from '../routes/diseaseRoutes.js';
-import orderRoutes from '../routes/orderRouts.js';
+import connectDB from './Config/db.js';
+import bonsaiRoutes from './routes/bonsaiRoutes.js';
+import treeRoutes from './routes/treeRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import errorHandler from './Midileware/errorMiddleware.js';
+import cartRoutes from './routes/cartRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import adminRoutes from './routes/adminRouts.js';
+import diseaseRoutes from './routes/diseaseRoutes.js';
+import orderRoutes from './routes/orderRouts.js';
 
 dotenv.config();
 connectDB();
